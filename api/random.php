104d08f1a9754935d589a407ac8ab556ef425f99
GoryMoon <gurreja@gmail.com>
2021-03-23 14:31:57 +0100

    Added an optional score limit query

--- api/random.php
+++ api/random.php
@@ -4,11 +4,29 @@ require_once '../init.php';
 use Loek\DB;
 use Loek\ReturnInfo;
 
+function numberOrDefault($raw, $default) {
+    if (isset($raw) && is_numeric($raw)) {
+        return $raw;
+    }
+    return $default;
+}
+
 if ($_SERVER['REQUEST_METHOD']=="GET") {
-    $sql = "SELECT * FROM `quotes` WHERE deleted = FALSE ORDER by RAND() LIMIT 1;";
-    $row = DB::pdo()->query($sql);
+    $scoreLimitLow = numberOrDefault($_GET['lowest'], 0);
+    $scoreLimitHigh = numberOrDefault($_GET['highest'],10000);
+
+    $query = DB::pdo()->prepare("SELECT * FROM `quotes` WHERE deleted = FALSE AND rating >= :low AND rating <= :high ORDER by RAND() LIMIT 1;");
+    $query->execute(array(
+        ':low' => $scoreLimitLow,
+        ':high' => $scoreLimitHigh
+    ));
     header('Content-Type:application/json;Charset:UTF8;');
-    echo json_encode(new ReturnInfo($row->fetch(PDO::FETCH_OBJ)));
+    $result = $query->fetch(PDO::FETCH_OBJ);
+    if ($result) {
+        echo json_encode(new ReturnInfo($result));
+    } else {
+        echo json_encode(new StdClass);
+    }
 } else {
     http_response_code(405);
 }
